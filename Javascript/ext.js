10fb26aa61e05744316734b63830b70cd9033d11
d-ksharma <151816731+d-ksharma@users.noreply.github.com>
2023-11-23 12:56:43 +0530

    Add files via upload

---
+++ Javascript/ext.js
@@ -0,0 +1,11 @@
+var a = "prince";
+let b = "Prince";
+let c = 21;
+let d = false;
+
+document.write("<h2>Javascript variables</h2>");
+document.write(a);
+document.write("<h1>",b,"</h1>");
+document.write(c,"<br>");
+c = 200;
+document.write(c);
